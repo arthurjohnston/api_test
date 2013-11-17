2029c9ea8aa864010df5316ea910630364c2678d
arthur.j.johnston@gmail.com <arthur.j.johnston@gmail.com>
2013-11-17 11:23:49 -0500

    changed limit to 7 days, made it runnable from repo dir

--- getpullrequests.py
+++ getpullrequests.py
@@ -1,9 +1,11 @@
-import git,datetime,re
+import git,datetime,re,os
+
 
 from git import *
+os.chdir('..')
 repo = Repo("api_test")
 pr="Merge pull request #"
-cutoff=int((datetime.datetime.now()-datetime.timedelta(days=21)).strftime('%s')); 
+cutoff=int((datetime.datetime.now()-datetime.timedelta(days=7)).strftime('%s')); 
 assert repo.bare == False
 for c in repo.iter_commits('master', max_count=500): #todo make these not magic numbers
 	if((c.committed_date>cutoff)and c.message.startswith(pr)):
